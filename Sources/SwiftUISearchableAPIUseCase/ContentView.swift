7e045514f9d3633c0b3434b53b5d0e97e03e3a37
Igor Malyarov <igor1309@gmail.com>
2022-09-23 00:34:33 +0300

    add `ContentView` and `ViewModel` from `swift-multi-picker`

--- Sources/SwiftUISearchableAPIUseCase/ContentView.swift
+++ Sources/SwiftUISearchableAPIUseCase/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Igor Malyarov on 23.09.2022.
 //
 
+import Combine
 import SwiftUI
 
 struct ContentView: View {
@@ -15,13 +16,53 @@ struct ContentView: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(viewModel.searchResults, content: searchResultView)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Title")
+        }
+        .searchable(
+            text: $viewModel.searchText,
+            placement: .navigationBarDrawer(displayMode: .always),
+            prompt: "Search"
+        )
+        .searchScopes($viewModel.scope) {
+            ForEach(ViewModel.SearchScope.allCases, id: \.self) { scope in
+                Text(scope.rawValue)
+                    .tag(scope)
+            }
+        }
+        .searchSuggestions {
+            ForEach(viewModel.searchSuggestions, content: searchSuggestionView)
+        }
+    }
+    
+    private func searchResultView(item: SearchResultItem) -> some View {
+        Text(item.title)
+            .font(.headline)
+    }
+    
+    private func searchSuggestionView(item: SearchSuggestionItem) -> some View {
+        Label(item.title, systemImage: item.icon)
+            .searchCompletion(item.text)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: .init())
+        ContentView(viewModel: .init(search: searchPublisher))
             .preferredColorScheme(.dark)
     }
 }
+
+private let searchPublisher: (String) -> ViewModel.SearchResultsPublisher = { string in
+    let filtered = [SearchResultItem].samples
+        .filter { $0.title.lowercased().hasPrefix(string.lowercased()) }
+    
+    return Just(filtered)
+        .eraseToAnyPublisher()
+}
+
+
--- Sources/SwiftUISearchableAPIUseCase/ViewModel.swift
+++ Sources/SwiftUISearchableAPIUseCase/ViewModel.swift
@@ -6,7 +6,71 @@
 //
 
 import Combine
+import Foundation
 
 final class ViewModel: ObservableObject {
+    @Published var searchText = ""
+    @Published private var suggestions = [SearchSuggestionItem].samples
+    @Published private(set) var searchResults = [SearchResultItem]()
+    @Published var scope: SearchScope = .crypto
     
+    typealias SearchResultsPublisher = AnyPublisher<[SearchResultItem], Never>
+    
+    init(search: @escaping (String) -> SearchResultsPublisher) {
+        $searchText
+            .flatMap(search)
+            .receive(on: DispatchQueue.main)
+            .assign(to: &$searchResults)
+    }
+    
+    var searchSuggestions: [SearchSuggestionItem] {
+        suggestions.filter {
+            $0.title.lowercased().hasPrefix(searchText.lowercased())
+        }
+    }
+
+//    var filteredMessages: [Message] {
+//        if searchText.isEmpty {
+//            return messages
+//        } else {
+//            return messages.filter { $0.text.localizedCaseInsensitiveContains(searchText) }
+//        }
+//    }
+    
+    enum SearchScope: String, CaseIterable {
+        case currency
+        case crypto
+        case stock
+        case derivative
+    }
+
+}
+
+struct SearchResultItem: Identifiable {
+    let title: String
+    
+    var id: String { title }
+}
+
+struct SearchSuggestionItem: Identifiable {
+    let title: String
+    let text: String
+    let icon: String
+    
+    var id: String { title }
+}
+
+extension Array where Element == SearchResultItem {
+    static let samples: Self = [
+        "aa", "aaa", "aaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa",
+        "bbbb", "ccc", "dd"]
+        .map(SearchResultItem.init)
+}
+
+extension Array where Element == SearchSuggestionItem {
+    static let samples: Self = [
+        .init(title: "aaa", text: "aaa", icon: "star"),
+        .init(title: "Aaa", text: "Aaaa aa aaaaaaaa", icon: "star"),
+        .init(title: "Bbb", text: "Bb bbbbbb bbb", icon: "scribble"),
+    ]
 }
