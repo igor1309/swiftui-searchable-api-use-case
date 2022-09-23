d2a86fdeb28df5c431a40bf98fe0c18665f245db
Igor Malyarov <igor1309@gmail.com>
2022-09-23 12:32:52 +0300

    extract static properties into separate file

---
+++ Sources/SwiftUISearchableAPIUseCase/Asset+ext.swift
@@ -0,0 +1,21 @@
+//
+//  Asset+ext.swift
+//  
+//
+//  Created by Igor Malyarov on 23.09.2022.
+//
+
+extension Array where Element == Asset {
+    static let samples: Self = [
+        .bitcoin, .etherium, .tether, .usdFuture, .usStock, .usdEur
+    ]
+}
+
+extension Asset {
+    static let bitcoin: Self = .init(title: "Bitcoin", text: "btcusd", type: .crypto)
+    static let etherium: Self = .init(title: "Etherium", text: "ethusd", type: .crypto)
+    static let tether: Self = .init(title: "Tether", text: "usdt", type: .crypto)
+    static let usdFuture: Self = .init(title: "USD Future", text: "usdf", type: .derivative)
+    static let usStock: Self = .init(title: "US Stock", text: "uss", type: .stock)
+    static let usdEur: Self = .init(title: "USD/EUR", text: "usdeur", type: .currency)
+}
