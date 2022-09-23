f2636e91f76aa12abaed090ffd34764d2e9b1db6
Igor Malyarov <igor1309@gmail.com>
2022-09-23 17:32:29 +0300

    add `SearchableAPIUseCasePreview` project

---
+++ App/SearchableAPIUseCasePreview/SearchableAPIUseCasePreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableAPIUseCasePreviewApp.swift
+//  SearchableAPIUseCasePreview
+//
+//  Created by Igor Malyarov on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SearchableAPIUseCasePreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
